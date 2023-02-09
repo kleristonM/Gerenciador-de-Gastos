170b8406c9ab806320f963bcaf108dfc68fec4e3
KleristonM <kleriston13@gmail.com>
2023-02-09 01:33:28 +0000

    Feat: alteração no extrato

--- app.js
+++ app.js
@@ -1,96 +1,30 @@
-// importar funções da livraria  do code.org para serem usadas na aplicação
-import {
-    onEvent, 
-    showElement, 
-    setScreen} from './lib/code.org.js';
+import { onEvent, setScreen } from './lib/code.org.js';
 
-// definir abaixo quais as variáveis que serão usadas
-
-
-// perguntar ao usuário quanto ele possui na carteira
 let carteira = prompt("QUANTO VOCÊ POSSUI NA CARTEIRA?"); 
-
-
-// perguntar ao usuario quanto ele quer economizar 
 let economia = prompt("QUANTO VOCÊ QUER ECONOMIZAR?");
-
-
-// a variavel extrato irá emitir uma listagem do que  foi gasto
-// ela deve iniciar vazia para elecar os gastos executaods
-let extrato = " ";
-
-
-// definir o valor que cada item custa
+let extrato = [];
 const comida = 18.00; 
 const circo = 15.00; 
 const brinquedo = 13.00;
 
-let listaDoExtrato = []
-
-
-// incluir o valor informado para gastos na carteira
 document.querySelector("#wallet").innerHTML = carteira;
 
-
-// cada vez que o botão for clicado ele deve: 
-onEvent("comida","click", () =>{
-    carteira -= comida; // reduzir o valor que foi declarado na variável
-    alert("Você gastou R$" + comida + " com comida!"); // informar ao usuário que ouve uma transação
-    extrato += "R$" + comida + " gastos com comida \n"; // armazenar em extrato o que está sendo gasto
-    document.querySelector("#wallet").innerHTML = carteira; // apresentar o valor restante em cateria deduzido do gasto
-    if (carteira <= economia){ // se o valor em carteira for inferior que a economia pretendida ele vai avisar com a msg e mudar o fundo da tela para vermelho
-        walletColorRed();
-        alert("ATENÇAO! Você atingiu seu limite de gastos");
-    }
-    let transacao = {
-        nome: "comida",
-        valor: 18,
-        data: new Date()
-    };
-    listaDoExtrato.push(transacao);
-});
-onEvent("brinquedo","click", () => {
-    carteira -= brinquedo;
-    alert("Você gastou R$" + brinquedo + " com brinquedo!");    
-    extrato += "R$" + brinquedo + " gastos com brinquedo \n"; 
+const addTransacao = (nome, valor) => {
+    carteira -= valor;
+    alert(`Você gastou R$ ${valor} com ${nome}!`);
+    extrato.push({ nome, valor, data: new Date() });
     document.querySelector("#wallet").innerHTML = carteira;
-    if (carteira < economia){
-        walletColorRed();
+    if (carteira < economia) {
+        document.querySelector("body").style.background = "linear-gradient(to bottom, #FF4040 0%, #FF4040 100%)";
         alert("ATENÇAO! Você atingiu seu limite de gastos");
     }
-    let transacao = {
-        nome: "brinquedo",
-        valor: 13,
-        data: new Date()
-    };
-    listaDoExtrato.push(transacao);
-});
-onEvent("circo","click", () => {
-    carteira -= circo; 
-    alert("Você gastou R$" + circo + " com circo!");    
-    extrato += "R$" + circo + " gastos com circo \n"; 
-    document.querySelector("#wallet").innerHTML = carteira;
-    if (carteira < economia){
-        walletColorRed();
-        alert("ATENÇAO! Você atingiu seu limite de gastos");
-    }
-    let transacao = {
-        nome: "circo",
-        valor: 15,
-        data: new Date()
-    };
-    listaDoExtrato.push(transacao);
-});
-console.log(listaDoExtrato);
-
-
-// quando clicar no botão EXTRATO ele vai abrir uma caixa de alerta listando os gastos e informando quando tem de saldo e qual o valor definido como economia.
-onEvent("go-extrato", "click", () => {    
-        alert(extrato + "\n" + "Seu saldo atual é de R$" + carteira + ".\n" + "Sua meta de gastos era de R$" + economia); 
-})
-
-
-// criar a variavel que altera o background do BODY para vermelho
-const walletColorRed = () => {
-   document.querySelector("body").style.background= "linear-gradient(to left, #660000, #990000, #ff0000)";   
 }
+
+onEvent("comida", "click", () => addTransacao("comida", comida));
+onEvent("brinquedo", "click", () => addTransacao("brinquedo", brinquedo));
+onEvent("circo", "click", () => addTransacao("circo", circo));
+
+onEvent("go-extrato", "click", () => {
+    let mensagem = extrato.map(t => `R$ ${t.valor} gastos com ${t.nome} em ${t.data.toLocaleString()}`).join("\n");
+    alert(`${mensagem}\nSeu saldo atual é de R$ ${carteira}.\nSua meta de gastos era de R$ ${economia}`); 
+});
